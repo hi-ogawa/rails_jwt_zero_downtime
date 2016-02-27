3b2c11565992940c20112a2a9561cc6ade021d1b
hiogawa <hi.ogawa.zz@gmail.com>
2016-02-28 03:17:25 +0900

    update cap setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,72 @@
-# config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'odigo_thai'
+set :repo_url, 'git@github.com:hi-ogawa/rails_jwt_zero_downtime.git'
+set :deploy_to, '/home/ubuntu/app'
+set :branch, ENV["BRANCH"] || "master"
+set :scm, :git
+set :format, :pretty
+set :log_level, :debug
+set :pty, false
+set :keep_releases, 5
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :linked_files, [
+  'config/database.yml',
+  'config/secrets.yml',
+  'config/settings.yml'
+]
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system']
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# capistrano-upload-config
+set :config_files, fetch(:linked_files)
 
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+## custom tasks ##
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  namespace :puma do
+    task :start do
+    end
+
+    task :stop do
+    end
+
+    task :restart do
+    end
+
+    task :status do
     end
   end
 
+  desc "place nginx config files to where nginx recognizes"
+  task :copy_nginx_config do
+    on roles(:web) do
+      upload! "config/system/nginx/#{fetch(:stage)}.conf", "#{release_path}/config/system/nginx" if ENV["COPY_FROM_LOCAL"]
+      execute "sudo ln -sf #{release_path}/config/system/nginx/#{fetch(:stage)}.conf /etc/nginx/sites-enabled/"
+    end
+  end
+  before :published, :copy_nginx_config
+end
+
+# usage: cap production invoke[db:seed]
+desc 'Invoke a rake command on the remote server'
+task :invoke, [:command] => 'deploy:set_rails_env' do |task, args|
+  on roles(:db) do
+    within release_path do
+      with :rails_env => fetch(:rails_env) do
+        rake args[:command]
+      end
+    end
+  end
+end
+
+def rsync(from, to)
+  "rsync -avz -e 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' --delete #{from} #{to}"
+end
+
+# for capistrano debug
+desc 'do nothing'
+task :do_nothing do
+  binding.pry
 end
