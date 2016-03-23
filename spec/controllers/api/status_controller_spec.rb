ca94fe2a03200a3a52b84b60c4da7330498dbbd5
hiogawa <hi.ogawa.zz@gmail.com>
2016-03-23 11:06:14 +0900

    make spec fails

--- spec/controllers/api/status_controller_spec.rb
+++ spec/controllers/api/status_controller_spec.rb
@@ -5,7 +5,7 @@ describe Api::StatusController, type: :controller do
   describe "get :show" do
     it "." do
       resp = get :show
-      expect(JSON.parse(resp.body)).to eq({"status" => 'ok'})
+      expect(JSON.parse(resp.body)).to eq({"status" => 'not-ok'})
       expect(resp.status).to eq(200)
     end
   end
